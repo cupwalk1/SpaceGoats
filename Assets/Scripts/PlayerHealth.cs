55d93fa6c84e683bdc4c016cbca64cb26a8e9151
Cupwalk2 <cooperwalkup@gmail.com>
2024-12-15 12:23:52 +0100

    Added player health.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<GameObject> obstacles= new();
+    [SerializeField] float oxygenLevel;
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
    private PlayerInputHandler playerInputHandler;
-
+   
    void Start()
    {
+      PlayerHealth playerHealth = GetComponent<PlayerHealth>();
       playerInputHandler = gameObject.AddComponent<PlayerInputHandler>();
       playerInputHandler.OnJump(new InputAction.CallbackContext());
    }
